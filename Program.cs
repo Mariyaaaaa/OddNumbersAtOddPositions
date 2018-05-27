be1abf9cfbffcdd09dec72bb0b15ca45a6e129d9
Mariya <mariqvalkova@gmail.com>
2018-05-27 09:09:19 +0300

    OddNumbersAtOddPositions

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var strings = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        for (int i = 1; i <= strings.Length; i += 2)
+        {
+            if (strings[i] % 2 != 0)
+            {
+                Console.WriteLine($"Index {i} -> {strings[i]}");
+            }
+        }
+
+
+    }
+
+
+}
+
